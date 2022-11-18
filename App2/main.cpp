0e8c09ea97a5aa93a139ed526937f62821322664
Martin Mittring <Kosmokleaner@Kosmokleaner.de>
2022-11-17 16:12:00 -0800

    Started KosmoRay2 from Dx12 tutorial
    https://github.com/jpvanoosten/LearningDirectX12

---
+++ App2/main.cpp
@@ -0,0 +1,48 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <Shlwapi.h>
+
+#include "DX12Lib/Application.h"
+#include "App2.h"
+
+#include <dxgidebug.h>
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "DXGI.lib")
+#pragma comment(lib, "Shlwapi.lib") // PathRemoveFileSpecW()
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "D3DCompiler.lib")
+
+void ReportLiveObjects()
+{
+//later    IDXGIDebug1* dxgiDebug;
+//later    DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug));
+
+//later    dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_IGNORE_INTERNAL);
+//later    dxgiDebug->Release();
+}
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
+{
+    int retCode = 0;
+
+    // Set the working directory to the path of the executable.
+    WCHAR path[MAX_PATH];
+    HMODULE hModule = GetModuleHandleW(NULL);
+    if ( GetModuleFileNameW(hModule, path, MAX_PATH) > 0 )
+    {
+        PathRemoveFileSpecW(path);
+        SetCurrentDirectoryW(path);
+    }
+
+    Application::Create(hInstance);
+    {
+        std::shared_ptr<Tutorial2> demo = std::make_shared<Tutorial2>(L"KosmoRay2 DX12", 1280, 720);
+        retCode = Application::Get().Run(demo);
+    }
+    Application::Destroy();
+
+    atexit(&ReportLiveObjects);
+
+    return retCode;
+}
